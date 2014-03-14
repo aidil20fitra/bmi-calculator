e559af208ae39a886fb7097be8247dee63dba760
Aditya Situmeang <bananab9001@gmail.com>
2014-03-14 11:13:31 +0700

    update 0.0.5

--- bmi.js
+++ bmi.js
@@ -1,13 +1,20 @@
 function hitungbmr(sex, berat, tinggi, umur){
+	var hasil = 1.0;
 	if (sex == 'f'){
-		return 655 + (9.6 * berat) + (1.8 * tinggi) - (4.7 * umur);
+		hasil = 655 + (9.6 * berat) + (1.8 * tinggi) - (4.7 * umur)
 	}else{
-		return 65 + (13.7 * berat) + (5 * tinggi) - (6.8 * umur);
+		hasil = 65 + (13.7 * berat) + (5 * tinggi) - (6.8 * umur);
 	}
+	console.log(hasil);
+	console.log(berat);
+	console.log(tinggi);
+	console.log(umur);
+
+	return hasil;
 }
 
 function hitungamr(bmr, aktivitas){
-	var persenan = array(
+	var persenan = new Array(
 		1.2,	// minim
 		1.375,	// ringan
 		1.55,	// normal
@@ -19,6 +26,7 @@ function hitungamr(bmr, aktivitas){
 }
 
 function hitungbmi(berat, tinggi){
+	tinggi = tinggi/100;
 	return berat / (tinggi * tinggi);
 }
 
